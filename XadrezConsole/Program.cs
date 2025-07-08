7570de4814fc35a5ed980aa327cdd7c8f1e03a8f
LuizEddie <contato.luiz1997@gmail.com>
2025-07-08 12:49:27 -0300

    feat: Começando a implementar o tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,12 +1,10 @@
-﻿using Tabuleiro;
+﻿using tabuleiro;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Posicao p = new Posicao(3, 4);
-
-        Console.WriteLine(p);
+        Tabuleiro tab = new Tabuleiro(8, 8);
 
         Console.ReadLine();
     }
