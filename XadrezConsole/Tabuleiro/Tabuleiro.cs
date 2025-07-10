0adcd4339391485d203e81505386ac9a8bd72380
LuizEddie <contato.luiz1997@gmail.com>
2025-07-10 10:24:11 -0300

    feat: Começando a colocar peças no tabuleiro

--- XadrezConsole/Tabuleiro/Tabuleiro.cs
+++ XadrezConsole/Tabuleiro/Tabuleiro.cs
@@ -22,5 +22,11 @@ namespace tabuleiro
         public Peca Peca(int linha, int coluna) { 
             return Pecas[linha, coluna];
         }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
     }
 }
